112333324d30341b6fa01b89edcd9ccb428fceae
rforde-esu <rforde@live.esu.edu>
2023-11-04 11:58:20 -0400

    save sitemap command changes

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,8 +4,6 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
-use Spatie\Sitemap\SitemapGenerator;
-use Spatie\Sitemap\Sitemap;
 use App\Models\County;
 use App\Models\City;
 use App\Models\Station;
@@ -32,16 +30,39 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
+        $date = Carbon::now()->format('Y-m-d');
+        $file = fopen(public_path().'/sitemap.xml','w');
+
+        $this->info('Writing to sitemap.xml in public folder...');
+
+        fwrite($file, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+        fwrite($file, "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n\n");
+
+        fwrite($file, $this->makeUrlSection('', $date, '1.0'));
+        fwrite($file, $this->makeUrlSection('search', $date, '0.9'));
+        fwrite($file, $this->makeUrlSection('stations', $date, '0.8'));
+        fwrite($file, $this->makeUrlSection('counties', $date, '0.8'));
+        fwrite($file, $this->makeUrlSection('cities', $date, '0.8'));
+
+        foreach (Station::all() as $station) {
+            fwrite($file, $this->makeUrlSection('stations/'.$station->station_name_slug, $date, '0.6'));
+        }
+        foreach (County::all() as $county) {
+            fwrite($file, $this->makeUrlSection('counties/'.$county->county_slug, $date, '0.5'));
+        }
+        foreach (City::all() as $city) {
+            fwrite($file, $this->makeUrlSection('cities/'.$city->city_slug, $date, '0.5'));
+        }
+
+        fwrite($file, "</urlset>");
         
-        $this->info('Crawling...');
-        //SitemapGenerator::create('http://127.0.0.1:8000/')->writeToFile(public_path().'sitemap.xml');
-        $map = Sitemap::create();
-        $map -> add(County::all());
-        $map -> writeToFile(public_path().'/sitemap.xml');
-        $this->newLine();
-        $this->info('Writing to sitemap.xml in public folder');
+        fclose($file);
         $this->newLine();
         $this->info('Sitemap made successfully!');
+    }
+
+    public function makeUrlSection($url_append, $date, $priority){
+        return "\t<url>\n\t\t<loc>https://pa-auto-inspections.com/".$url_append."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<priority>".$priority."</priority>\n\t</url>\n\n";
     }
 }
 
